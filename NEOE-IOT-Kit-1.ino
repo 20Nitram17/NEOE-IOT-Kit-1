4ad419c60a11a95c2584e7f048300b1701f208bd
Julian Bradler <48619598+bradlerj@users.noreply.github.com>
2020-11-01 00:38:03 +0100

    Update NEOE-IOT-Kit-1.ino

--- NEOE-IOT-Kit-1.ino
+++ NEOE-IOT-Kit-1.ino
@@ -1,8 +1,9 @@
 /**********************************************************************************************************************************
   Arduino-Sketch für das NEOE-IOT-Kit-1, "Smart Home Luftqualitätssensor mit MQ-135 und NodeMCU. Reagiert auf CO2 und andere Gase. 
-  Arduino-Programmierung. MQTT-kompatibel." 
-  https://www.neoe.io/products/smarthome-iot-bausatz-luftqualitatssensor-mq-135-nodemcu-arduino-breadbord
-  https://www.neoe.io/products/smarthome-iot-bausatz-luftqualitatssensor-mq-135-nodemcu-arduino-pcb
+  Arduino-Programmierung. MQTT-kompatibel zur Anbindung an Home Assistant."
+  Dieser Arduino-Sketch wird in den folgenden beiden Tutorials verwendet.
+  https://www.neoe.io/blogs/tutorials/luftqualitatssensor-reagiert-auf-co2-mqtt-kompatibel-aufbau-variante-breadboard
+  https://www.neoe.io/blogs/tutorials/luftqualitatssensor-reagiert-auf-co2-mqtt-kompatibel-aufbau-variante-leiterplatte
   Fragen und Anregungen bitte in unserer Facebook-Gruppe adressieren, damit die gesamte Community davon profitiert. 
   https://www.facebook.com/groups/neoe.io/
   Datum der letzten Änderung: 27. Oktober, 2020
