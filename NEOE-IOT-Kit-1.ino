1a3d8ac3d3a860f846fd341b5c47525810d3967d
Julian Bradler <48619598+bradlerj@users.noreply.github.com>
2020-11-02 18:01:45 +0100

    Update NEOE-IOT-Kit-1.ino
    Kleinere Optimierungen

--- NEOE-IOT-Kit-1.ino
+++ NEOE-IOT-Kit-1.ino
@@ -1,10 +1,10 @@
 /**********************************************************************************************************************************
-  Arduino-Sketch für das NEOE-IOT-Kit-1, "Smart Home Luftqualitätssensor mit MQ-135 und NodeMCU. Reagiert auf CO2 und andere Gase. 
+  Arduino-Sketch für das NEOE-IOT-Kit-1, "Smart Home Luftqualitätssensor mit MQ-135 und NodeMCU. Reagiert auf CO2 und andere Gase.
   Arduino-Programmierung. MQTT-kompatibel zur Anbindung an Home Assistant."
-  Dieser Arduino-Sketch wird in den folgenden beiden Tutorials verwendet.
+  Dieser Arduino-Sketch wird in den folgenden beiden Tutorials verwendet:
   https://www.neoe.io/blogs/tutorials/luftqualitatssensor-reagiert-auf-co2-mqtt-kompatibel-aufbau-variante-breadboard
   https://www.neoe.io/blogs/tutorials/luftqualitatssensor-reagiert-auf-co2-mqtt-kompatibel-aufbau-variante-leiterplatte
-  Fragen und Anregungen bitte in unserer Facebook-Gruppe adressieren, damit die gesamte Community davon profitiert. 
+  Fragen und Anregungen bitte in unserer Facebook-Gruppe adressieren, damit die gesamte Community davon profitiert.
   https://www.facebook.com/groups/neoe.io/
   Datum der letzten Änderung: 27. Oktober, 2020
 **********************************************************************************************************************************/
@@ -61,18 +61,15 @@ const char* mqtt_user = "BENUTZERNAME"; // Anführungszeichen beibehalten
 const char* mqtt_password = "PASSWORT"; // Anführungszeichen beibehalten, also z.B. so: "Geheim"
 
 // MQTT-Topic für Home Assistant MQTT Auto Discovery
-const char* mqtt_config_topic = "homeassistant/sensor/NEOE-IOT-KIT-1-1/config";
-const char* mqtt_state_topic = "homeassistant/sensor/NEOE-IOT-KIT-1-1/state";
-
-// MQTT-Version
-#define MQTT_VERSION MQTT_VERSION_3_1_1
+const char* mqtt_config_topic = "homeassistant/sensor/co2-esszimmer/config"; // Name des Zimmers bei Bedarf ändern
+const char* mqtt_state_topic = "homeassistant/sensor/co2-esszimmer/state"; // Name des Zimmers bei Bedarf ändern
 
 // Speicher-Reservierung für JSON-Dokument, kann mithilfe von arduinojson.org/v6/assistant noch optimiert werden
-StaticJsonDocument<500> doc_config;
-StaticJsonDocument<500> doc_state;
+StaticJsonDocument<512> doc_config;
+StaticJsonDocument<512> doc_state;
 
-char mqtt_config_data[500];
-char mqtt_state_data[500];
+char mqtt_config_data[512];
+char mqtt_state_data[512];
 
 bool configured = false;
 
@@ -86,14 +83,14 @@ void publishData(float p_co2) {
   client.publish(mqtt_state_topic, mqtt_state_data);
 }
 
-// Funktion zur Verbindung mit dem WLAN
+// Funktion zur WLAN-Verbindung
 void setup_wifi() {
   delay(10);
+  /* Mit WLAN verbinden */
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
   }
-  randomSeed(micros());
 }
 
 // Funktion zur MQTT-Verbindung
@@ -104,6 +101,17 @@ void reconnect() {
       delay(5000);
     }
   }
+}
+
+// Funktion für Home Assistant MQTT Auto Discovery
+void configMqtt() {
+  doc_config["name"] = "CO2-Wert Esszimmer";  // Name des Zimmers bei Bedarf ändern
+  doc_config["state_topic"] = mqtt_state_topic;
+  doc_config["unit_of_measurement"] = "ppm";
+  doc_config["value_template"] = "{{ value_json.co2}}";
+  serializeJson(doc_config, mqtt_config_data);
+  client.publish(mqtt_config_topic, mqtt_config_data, true);
+  delay(1000);
 }
 
 // Widerstand des Sensors (in Kiloohm) berechnen
@@ -123,38 +131,22 @@ float getRZero() {
 }
 
 void setup() {
-
   // Array zur Berechnung der Durchschnittswerte aufbauen
   for (int thisReading = 0; thisReading < numReadings; thisReading++) {
     readings[thisReading] = 0;
   }
-
   setup_wifi();
   client.setServer(mqtt_server, mqtt_port);
   client.setBufferSize(512);
-  
+  if (!client.connected()) reconnect();
+  configMqtt();
 }
 
 void loop() {
 
-  if (!client.connected()) {
-    reconnect();
-  }
+  if (!client.connected()) reconnect();
 
-  if (configured==false){
-  doc_config["name"] = "CO2-Sensor";
-  doc_config["state_topic"] = "homeassistant/sensor/NEOE-IOT-KIT-1-1/state";
-  doc_config["unit_of_measurement"] = "ppm";
-  doc_config["value_template"] = "{{ value_json.co2}}";
-  serializeJson(doc_config, mqtt_config_data);
-  // aktuell können nur QOS 0 Nachrichten verschickt werden, daher senden wir die Nachricht sicherheitshalber drei Mal.
-  for ( int x = 0; x < 2; x++ ){
-  client.publish(mqtt_config_topic, mqtt_config_data, true);
-  delay(1000);
-  }
-  configured = true;
-  }
-  // Sensor auslesen und Durchschnittswerte berechnen 
+  // Sensor auslesen und Durchschnittswerte berechnen
   total = total - readings[readIndex]; readings[readIndex] = analogRead(_pin); total = total + readings[readIndex]; readIndex = readIndex + 1; if (readIndex >= numReadings) {
     readIndex = 0;
   }
@@ -163,7 +155,7 @@ void loop() {
   // Volatiler Sensor, daher sanfte Kalibrierung, RCurrent nur mit 0,001%
   float RCurrent = getRZero();
   if ((((9999 * RMax) + RCurrent) / 10000) > RMax) {
-    RMax = (((9999 * RMax) + RCurrent) / 10000); 
+    RMax = (((9999 * RMax) + RCurrent) / 10000);
   }
 
   // ppm berechnen
@@ -178,15 +170,15 @@ void loop() {
   LEDgreen = 1024 - ((1024 * ((ppm - 500) / 500)) / 3);
   if (LEDred > 1024) LEDred = 1024;
   if (LEDred < 0) LEDred = 0; if (LEDgreen > 1024)LEDgreen = 1024;
-  if (LEDgreen < 0) LEDgreen = 0; 
+  if (LEDgreen < 0) LEDgreen = 0;
 
   // Indikator-LED schaltet von Blau (Kalibrierung) auf Grün, sobald Daten im 95% Bereich
   if (ppm < ATMOCO2) {
     LEDgreen = 0;
     LEDred = 0;
     LEDblue = 1024;
-  } 
-  
+  }
+
   // Nach erfolgter Kalibrierung Anzahl der Messungen reduzieren, da sonst zu viele MQTT-Meldungen versendet werden
   if (ppm > ATMOCO2) delay(delay_time);
 
@@ -194,5 +186,5 @@ void loop() {
   analogWrite(PINgreen, LEDgreen);
   analogWrite(PINred, LEDred);
   analogWrite(PINblue, LEDblue);
- 
+
 }
